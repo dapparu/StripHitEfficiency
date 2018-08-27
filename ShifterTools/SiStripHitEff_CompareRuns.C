720be950dfc59f837f9a1ff5b288dd8ba6385ee0
jlagram <jlagram@cern.ch>
2018-08-27 11:06:52 +0200

    Added lumi info in the legend

--- ShifterTools/SiStripHitEff_CompareRuns.C
+++ ShifterTools/SiStripHitEff_CompareRuns.C
@@ -6,6 +6,7 @@
 #include <TStyle.h>
 #include <TGraphAsymmErrors.h>
 #include <iostream>
+#include <TLatex.h>
 
 void bookGraph (TGraphAsymmErrors *g, int nL);
 void SiStripHitEff_CompareRuns(TString ERA1, TString runnumber1, TString ERA2, TString runnumber2);
@@ -32,19 +33,45 @@ void SiStripHitEff_CompareRuns(TString ERA1, TString runnumber1, TString ERA2, T
     return;
   }  
   
+  // Get histos and lumi info
   f1->cd("SiStripHitEff");
   found_r1=(TH1F*)gDirectory->Get("found");
   found2_r1=(TH1F*)gDirectory->Get("found2");
   all_r1=(TH1F*)gDirectory->Get("all");
   all2_r1=(TH1F*)gDirectory->Get("all2");
+  TH1F *lumi1=(TH1F*)gDirectory->Get("instLumi");
+  TH1F *pu1=(TH1F*)gDirectory->Get("PU");
+
+  double lum1=lumi1->GetMean();
+  int puissance1=30;
+  while(lum1>=10.)
+    {
+      lum1/=10.;
+      puissance1++;
+    }
+  TString meanlumi1=Form("%.2f",lum1);
+  TString meanpu1=Form("%.2f",pu1->GetMean());
   
   f2->cd("SiStripHitEff");
   found_r2=(TH1F*)gDirectory->Get("found");
   found2_r2=(TH1F*)gDirectory->Get("found2");
   all_r2=(TH1F*)gDirectory->Get("all");
   all2_r2=(TH1F*)gDirectory->Get("all2");
+  TH1F *lumi2=(TH1F*)gDirectory->Get("instLumi");
+  TH1F *pu2=(TH1F*)gDirectory->Get("PU");
 
+  double lum2=lumi2->GetMean();
+  int puissance2=30;
+  while(lum2>=10.)
+    {
+      lum2/=10.;
+      puissance2++;
+    }
+  TString meanlumi2=Form("%.2f",lum2);
+  TString meanpu2=Form("%.2f",pu2->GetMean());
 
+  
+  // Compute efficiencies, fill graphs
   TGraphAsymmErrors *gr_r1 = new TGraphAsymmErrors(nLayers+1);
   gr_r1->BayesDivide(found_r1,all_r1); 
   bookGraph(gr_r1, nLayers);
@@ -71,30 +98,32 @@ void SiStripHitEff_CompareRuns(TString ERA1, TString runnumber1, TString ERA2, T
   gr2_r2->SetLineColor(1);
   gr2_r2->SetMarkerStyle(25);
   
+  
+  // Draw plots
   TCanvas *c = new TCanvas();
   c->SetGridy();
   c->SetGridx();
   gr_r1->Draw("AP");
   gr_r2->Draw("Psame");
-  TLegend *leg1 = new TLegend(0.40,0.15,0.88,0.40);
+  TLegend *leg1 = new TLegend(0.4,0.205,0.89,0.355);
   leg1->SetFillStyle(0);
-  leg1->SetBorderSize(0);
-  leg1->AddEntry(gr_r1,"Good Modules, RUN" + runnumber1,"p");
-  leg1->AddEntry(gr_r2,"Good Modules, RUN" + runnumber2,"p");
-  leg1->SetTextSize(0.030);
-  leg1->SetFillColor(0);
+  //leg1->SetBorderSize(50);
+  leg1->AddEntry(gr_r1,Form("#splitline{Good Modules, RUN %s}{<PU>: %s, <inst. lumi.>: %s x 10^{%i} cm^{-2} s^{-1}}",runnumber1.Data(),meanpu1.Data(),meanlumi1.Data(),puissance1),"p");
+  leg1->AddEntry(gr_r2,Form("#splitline{Good Modules, RUN %s}{<PU>: %s, <inst. lumi.>: %s x 10^{%i} cm^{-2} s^{-1}}",runnumber2.Data(),meanpu2.Data(),meanlumi2.Data(),puissance2),"p");
+  leg1->SetTextSize(0.027);
+  leg1->SetFillStyle(1001);
   leg1->Draw("same");
   c->SaveAs("SiStripHitEff_CompareRuns_GoodModules.png","png");
+
   gr2_r1->Draw("AP");
   gr2_r2->Draw("Psame");
-  TLegend *leg2 = new TLegend(0.40,0.15,0.88,0.40);
+  TLegend *leg2 = new TLegend(0.4,0.205,0.89,0.355);
   leg2->SetFillStyle(0);
-  leg2->SetBorderSize(0);
-  leg2->SetTextSize(1.);
-  leg2->AddEntry(gr2_r1,"All Modules, RUN" + runnumber1,"p");
-  leg2->AddEntry(gr2_r2,"All Modules, RUN" + runnumber2,"p");
-  leg2->SetTextSize(0.030);
-  leg2->SetFillColor(0);
+  //leg2->SetBorderSize(0);
+  leg2->AddEntry(gr2_r1,Form("#splitline{All Modules, RUN %s}{<PU>: %s, <inst. lumi.>: %s x 10^{%i} cm^{-2} s^{-1}}",runnumber1.Data(),meanpu1.Data(),meanlumi1.Data(),puissance1),"p");
+  leg2->AddEntry(gr2_r2,Form("#splitline{All Modules, RUN %s}{<PU>: %s, <inst. lumi.>: %s x 10^{%i} cm^{-2} s^{-1}}",runnumber2.Data(),meanpu2.Data(),meanlumi2.Data(),puissance2),"p");
+  leg2->SetTextSize(0.027);
+  leg2->SetFillStyle(1001);
   leg2->Draw("same"); 
   c->SaveAs("SiStripHitEff_CompareRuns_AllModules.png","png");
 
@@ -108,6 +137,7 @@ void bookGraph (TGraphAsymmErrors *g, int nL){
     g->SetPointError(j, 0., 0., g->GetErrorYlow(j),g->GetErrorYhigh(j) );
 
   g->GetXaxis()->SetLimits(0,nL);
+  g->GetYaxis()->SetLimits(0.88,1.0);
   g->SetMarkerSize(1.2);
   g->SetLineWidth(4);
   g->SetMinimum(0.90);
